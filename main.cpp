fe63aa85d27a770d5e8961920d54579556ef3c66
andre <andreiprodan242@gmail.com>
2025-03-23 17:51:35 +0200

    test1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 class produs {
     double pret;
@@ -9,7 +10,7 @@ class produs {
 public:
     explicit produs(const double pret_ = 0, std::string nume_ = "NULL",
                     const int cantitate_ = 0, const int id_raion_ = 0)
-        : pret(pret_), nume(std::move(nume_)), cantitate(cantitate_), id_raion(id_raion_) {}
+        : pret(pret_), nume(std::move(nume_)), cantitate(cantitate_), id_raion(id_raion_) {std::cout<<nume<<" s-a initializat cu succes!\n";}
 
     ~produs() = default;
 
@@ -30,10 +31,27 @@ public:
     }
 };
 
+class raion {
+    int id;
+    std::string nume;
+    std::vector<produs> produse;
+public:
+    explicit raion(const int id_ = 0, std::string nume_ = "NULL", const std::vector<produs>& produse_ = {}) : id(id_), nume(std::move(nume_)), produse(produse_) {
+        std::cout<<"Raionul pentru "<<nume<<" s-a initializat cu succes!\n";
+    }
+    void afiseazaProduse() const {
+        std::cout << "Produse in raionul " << nume << ":\n";
+        for (const produs& p : produse) {
+            std::cout << p;
+        }
+    }
+};
+
 int main() {
     produs p1(14.5, "Branza Hochland", 5, 3);
-    produs p2(10.7, "Coca-Cola Zero", 20, 2);
+    produs p2(10.7, "Coca-Cola Zero", 20, 2), p3(21.5, "Foietaje cu feta", 10, 1), p4(15, "Pastrav", 15, 1);
     std::cout << p1 << p2;
-
+    raion r1(1, "congelate", std::vector<produs>{p3,p4});
+    r1.afiseazaProduse();
     return 0;
 }
